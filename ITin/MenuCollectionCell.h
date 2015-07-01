12c52e643fbf9edbbf588135b1785d0dc7dc71c5
Mac <mac@Maui.attlocal.net>
2015-07-01 11:58:08 -0400

    Guillermo : First Menu approach ready, beginning of the Itinerary table

---
+++ ITin/MenuCollectionCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuCollectionCell.h
+//  ITin
+//
+//  Created by Mac on 7/1/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuCollectionCell : UICollectionViewCell
+
+@property ( nonatomic, weak ) IBOutlet UIButton *btnMCCell;
+
+@end
---
+++ ITin/MenuCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MenuCollectionCell.m
+//  ITin
+//
+//  Created by Mac on 7/1/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import "MenuCollectionCell.h"
+
+@implementation MenuCollectionCell
+
+@end
