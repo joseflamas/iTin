f547c8559afe23200ace45b2053cc4e309faa8f3
Mac <mac@Maui.attlocal.net>
2015-06-30 20:45:59 -0400

    Initial Commit

---
+++ ITin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITin
+//
+//  Created by Mac on 6/30/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
