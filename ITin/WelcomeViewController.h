7cc8a03abb841c768f4a991603f8ebb6ce20bf7a
Mac <mac@Maui.attlocal.net>
2015-06-30 21:20:45 -0400

    Change the initial View Controller to Welcome View Controller

---
+++ ITin/WelcomeViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  ITin
+//
+//  Created by Mac on 6/30/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+/*
+ 
+    Welcome View:
+ 
+    This view takes charges of figuring out if this is your first run and take to the first survey or if not
+    prepares the app with your coordinates and the preferences that the user already gave.
+
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+
+@end
+
