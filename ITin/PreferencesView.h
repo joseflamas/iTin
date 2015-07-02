a2d898095f15553c168d74ca61eddfefc329b1e3
Mac <mac@MacBook-Pro.attlocal.net>
2015-07-02 03:02:39 -0400

    Jamaal - added two new files. made changes to plist

---
+++ ITin/PreferencesView.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesView.h
+//  ITin
+//
+//  Created by Mac on 7/2/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreferencesView : UIScrollView
+
+@end
---
+++ ITin/PreferencesView.m
@@ -0,0 +1,21 @@
+//
+//  PreferencesView.m
+//  ITin
+//
+//  Created by Mac on 7/2/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import "PreferencesView.h"
+
+@implementation PreferencesView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
